4e45d133a0f9e90e108a86b6d3c26a1a6a7b2024
bahram.aliyev <bahram.aliyev@gmail.com>
2017-11-26 21:21:38 -0600

    Added .net standard implementation of messaging contracts.

---
+++ src/MessageBrokerWebApi/PetTracking.Contracts/V1/TrackingBatch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ENSE483Group3Fall2017.PetTracking.Contracts.V1
+{
+    public class TrackingBatch
+    {
+        public Guid Id { get; set; }
+
+        public string GpsCoordinates { get; set; }
+
+        public DateTime FrameStart { get; set; }
+
+        public DateTime FrameEnd { get; set; }
+
+        public IEnumerable<TrackingItem> Items { get; set; }
+    }
+}
---
+++ src/MessageBrokerWebApi/PetTracking.Contracts/V1/TrackingItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ENSE483Group3Fall2017.PetTracking.Contracts.V1
+{
+    public class TrackingItem
+    {
+        public Guid Id { get; set; }
+
+        public string BeaconId { get; set; }
+
+        public decimal Proximity { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
